2a667789a6e41fe188cfcb65b10b2e91028c6dd1
Subhash Birajdar <subhashchandrabirajdar98@gmail.com>
2024-07-31 21:29:14 +0530

    Spring Validation on UserDto to User and UserController

--- src/main/java/com/Blog_App_Apis/Payload/UserDto.java
+++ src/main/java/com/Blog_App_Apis/Payload/UserDto.java
@@ -1,5 +1,8 @@
 package com.Blog_App_Apis.Payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,13 +11,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
+
     private int id;
 
+    //@NotEmpty - it provide @NotNUll + @NotBlank
+    @NotEmpty
+    @Size(min = 4, message = "username must be min of 4 character !!!")
     private String name;
 
+    @Email(message = "email address is not valid !!!")
     private String email;
 
+    @NotEmpty
+    @Size(min = 4, max = 8, message = "Password must be min of 4 chars and max of 8 char !!! ")
     private String password;
 
+    @NotEmpty
     private String about;
 }
--- src/main/java/com/Blog_App_Apis/controller/UserController.java
+++ src/main/java/com/Blog_App_Apis/controller/UserController.java
@@ -3,6 +3,7 @@ package com.Blog_App_Apis.controller;
 import com.Blog_App_Apis.Payload.UserDto;
 import com.Blog_App_Apis.exception.ApiResponse;
 import com.Blog_App_Apis.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/addUser") // http://localhost;8080/api/users/
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto){
         UserDto CreateuserDto1 = this.userService.createUser(userDto);
         return new ResponseEntity<>(CreateuserDto1, HttpStatus.CREATED);
     }
@@ -35,7 +36,7 @@ public class UserController {
      */
 
     @PutMapping("/{userId}")  //http://localhost;8080/api/users/1
-    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto,
+    public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto,
                                               @PathVariable("userId") Integer Id)
     {
         UserDto updateduserDto1 = this.userService.updateUser(userDto, Id);
