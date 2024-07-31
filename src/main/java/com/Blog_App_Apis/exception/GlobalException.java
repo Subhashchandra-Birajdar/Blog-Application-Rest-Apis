9598895e947ee4d20679dc15d5c027c1640a0718
Subhash Birajdar <subhashchandrabirajdar98@gmail.com>
2024-07-31 21:31:51 +0530

    Handled MethodArgumentNotValidException in GlobalException class

--- src/main/java/com/Blog_App_Apis/exception/GlobalException.java
+++ src/main/java/com/Blog_App_Apis/exception/GlobalException.java
@@ -2,6 +2,8 @@ package com.Blog_App_Apis.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,5 +19,28 @@ public class GlobalException {
         ApiResponse apiResponse = new ApiResponse(message,false);
         return  new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
     }
+
+    //two ways we can write logic here
+//1.way
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<Map<String,String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+//         Map<String,String> response = new HashMap<>();
+//         ex.getBindingResult().getAllErrors().forEach((error)-> {
+//             String fieldName = ((FieldError) error).getField();
+//             String message = error.getDefaultMessage();
+//             response.put(fieldName,message);
+//         });
+//         return new ResponseEntity<Map<String,String>>(response,HttpStatus.BAD_REQUEST);
+//    }
+//2.way
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        System.out.println("Handling method argument not valid exception");
+        return ex.getBindingResult()
+                .getAllErrors().stream()
+                .map(ObjectError::getDefaultMessage)
+                .collect(Collectors.toList());
+    }
+
 }
 
