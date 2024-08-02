c7e6f09f134f49438c8c79e4a37d4333e81b2a16
Subhash Birajdar <subhashchandrabirajdar98@gmail.com>
2024-08-02 20:25:23 +0530

    Applied Validation on CategoryDto

--- src/main/java/com/Blog_App_Apis/Payload/CategoryDto.java
+++ src/main/java/com/Blog_App_Apis/Payload/CategoryDto.java
@@ -1,4 +1,6 @@
 package com.Blog_App_Apis.Payload;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +9,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryDto {
+
+    @NotBlank
+    @Size(min=4,message = "Min size of cateogry title is 4")
     private String categoryTitle;
+
+    @NotBlank
+    @Size(min=10,message="min size of category desc is 10")
     private String categoryDescription;
 }
